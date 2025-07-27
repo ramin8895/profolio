c10ac44876d2032f89883f0239c4f6a17a06c85f
Ramin <yourEmail>
2025-07-27 22:19:56 +0330

    add my project

---
+++ components/projectsComponents/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+
+const ProjectsComponents = () => {
+  return (
+    <section className="py-12  ">
+      <div className="text-center mb-10">
+        <h2 className="text-3xl font-bold text-gray-800 sm:text-4xl">
+          My Projects
+        </h2>
+        <p className="mt-2 text-gray-500">
+          Some of the projects I’ve worked on
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <div className="bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition duration-300">
+          <Image
+            width={400}
+            height={250}
+            alt="Calendro Project"
+            src="/proimage/calendro.png"
+            className="w-full h-auto object-cover"
+          />
+          <div className="p-4">
+            <h3 className="text-xl font-semibold text-gray-800">Calendro</h3>
+            <p className="text-gray-500 mt-1 text-sm">
+              A modern calendar app for productivity and scheduling.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsComponents;
